2562de87ba521cb257720dd0548455a78e1a35cc
Joice-Simao <89137612+Joice-Simao@users.noreply.github.com>
2021-09-28 10:25:55 -0300

    blog pessoal

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,17 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	/*
+	 * essa method Query eh equivalente a: select * from tb_postagem  where titulo like %titulo%
+	 * 
+	 */
+	 
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+}
